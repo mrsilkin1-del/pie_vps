d95905da4cef39e3128575f07226b3351ad50688
mrsilkin1-del <mr.silkin1@fasdeil.com>
2026-05-10 12:10:32 +0700

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,52 +1,43 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
+  # Which nixpkgs channel to use
   channel = "stable-24.05"; # or "unstable"
 
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.python312
-    pkgs.python312Packages.pip
+  # Packages to be installed in the development environment
+  packages = with pkgs; [
+    unzip
+    openssh
+    git
+    qemu_kvm
+    sudo
+    cdrkit
+    cloud-utils
+    qemu
   ];
 
-  # Sets environment variables in the workspace
-  env = {};
+  # Environment variables for the workspace
+  env = {
+    # Example: set default editor
+    EDITOR = "nano";
+  };
+
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    # Extensions from https://open-vsx.org (use "publisher.id")
     extensions = [
-      # "vscodevim.vim"
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
     ];
 
-    # Enable previews
-    previews = {
-      enable = true;
-      previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
-      };
-    };
-
-    # Workspace lifecycle hooks
     workspace = {
       # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
+      onCreate = { };
+
+      # Runs each time the workspace is (re)started
+      onStart = { };
+    };
+
+    # Disable previews completely
+    previews = {
+      enable = false;
     };
   };
 }
